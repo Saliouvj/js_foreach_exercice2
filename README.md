17de72e36917b118cb2394320bb64b6d99908abf
saliouvj <m.saliou90@gmail.com>
2021-01-21 10:23:34 +0100

    decla

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # js_foreach_exercice2
-# js_foreach_exercice2
+
---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+// EXO 1
+
+let tab = ["un", "deux", "trois", "quatre", "cinq"]
+
+tab.forEach(element => {
+    console.log(element);
+});
+
+// EXO 2
+
+let coding16 = ["saliou", "zakaria", "ayoub", "mohammed", "alex", "adame", "sebastien", "achraf", "bene", "abdellah", "malo", "louise", "natchez", "dawid", "maximilien", "jamila", "maxence", "zulma"]
+
+coding16.forEach((element, i) => {
+    console.log(`Bonjour ${element} ${[i]}`);
+});
+
+// coding16.forEach((el, i, panier) => {
+//     console.log(el, i, panier);
+// });
